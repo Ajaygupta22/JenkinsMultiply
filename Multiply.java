b48c24f31e22758515a870644c92d9990da55fed
Ajaygupta22 <spg22022000@gmail.com>
2022-04-28 11:07:56 +0530

    Update

--- Multiply.java
+++ Multiply.java
@@ -3,7 +3,8 @@ class Multiply{
 		int a = 10;
 		int b = 5;
 		int ans = a*b;
-		System.out.print("The answer is: ");
-		System.out.print(ans);
+		System.out.println("The first number is: "+a);
+		System.out.println("The second number is: "+b);
+		System.out.println("The answer is: "+ans);
 	}
 }
